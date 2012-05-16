2e9310bd194cff7357bc1276dd781012169d05e7
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-05-16 13:34:37 +0400

    Added a script using bibmanage to extract information from the .bib files. Bibmanage can be installed at
    http://packages.python.org/biblio-py/bibmanage.html

--- my_simple_script.py
+++
@@ -1,7 +0,0 @@
-import yapbib.biblist as biblist
-b=biblist.BibList()
-b.import_bibtex('our.bib')
-
-#Create a reduced database
-items=b.search(findstr='Oseledets',fields=['author'])
-
---
+++ simple.sh
@@ -0,0 +1,2 @@
+bibmanage.py --search=Tyrtyshnikov:author -b 2007 -e 2012 --keep-keys --sort=year our.bib -o tee.tex -f tex
+
