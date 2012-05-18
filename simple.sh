5caca9646bb6fbdbd7d080e0dfe7dea9755f1c9e
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-05-18 12:19:53 +0400

    It was a bug there

--- simple.sh
+++ simple.sh
@@ -1,2 +1,2 @@
-bibmanage.py --search=Tyrtyshnikov:author -b 2007 -e 2012 --keep-keys --sort=year our.bib -o tee.tex -f tex
+bibmanage.py --search=Oseledets:author -b 2007 -e 2012 --keep-keys --sort=year our.bib -o oseledets.tex -f tex
 
