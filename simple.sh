abcf72bda6f91fdd1c388b910539edcbac2745fc
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-06-05 13:13:59 +0400

    Some references to DMRG added + two fixes

--- simple.sh
+++ simple.sh
@@ -1,2 +1,2 @@
-bibmanage.py --search=Oseledets:author -b 2007 -e 2012 --keep-keys --sort=year our.bib -o oseledets.tex -f tex
+bibmanage.py --search=Oseledets:author -b 2007 -e 2012 --keep-keys --sort=year our.bib -o oseledets.bib -f bib
 
