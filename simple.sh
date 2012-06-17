322e2c29f0d6258b83bf907d1ce5cba384193769
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-06-17 20:47:01 +0400

    Year should be integer! (without (doi) stuff). It is needed by some parsers

--- simple.sh
+++ simple.sh
@@ -1,2 +1,2 @@
-bibmanage.py --search=Oseledets:author -b 2007 -e 2012 --keep-keys --sort=year our.bib -o oseledets.bib -f bib
+bibmanage.py --search=Oseledets:author -b 2003 -e 2012 --keep-keys --sort=year our.bib -o oseledets.bib -f bib
 
