5d65b982c3fba9ec3e328436ac26aca3d5dbe6f1
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-07-16 20:51:05 +0200

    It works, now css!

--- gen_bib.sh
+++ gen_bib.sh
@@ -1,8 +1,12 @@
-bib2bib -ob "cur_preprints.bib" -c '$type : "techreport" and not exists newversion' our.bib 
-bib2bib -ob "published.bib" -c '$type : "article" or $type : "incollection"' our.bib
-bibtex2html -s "plain" published.bib
-bibtex2html -s "plain" cur_preprints.bib
-echo "<h1>Published papers</h1>" > published_tail.html
-cat published.html | tail -n +16 >> published_tail.html
-cat cur_preprints.html | grep -v "</body>" | grep -v "</html>" | sed 's/<title>cur_preprints<\/title>/<h1>Recent Preprints<\/h1>/g' > cur_preprints_head.html
-cat cur_preprints_head.html published_tail.html > all_our.html
+bib2bib -s year -ob "cur_preprints.bib" -c '$type : "techreport" and not exists newversion' our.bib 
+bib2bib -s year -ob "published.bib" -c '$type : "article" or $type : "incollection"' our.bib
+#bibtex2html -s "plain" published.bib
+#bibtex2html -s "plain" cur_preprints.bib
+#echo "<h1>Published papers</h1>" > published_tail.html
+#cat published.html | tail -n +16 >> published_tail.html
+#cat cur_preprints.html | grep -v "</body>" | grep -v "</html>" | sed 's/<title>cur_preprints<\/title>/<h1>Recent Preprints<\/h1>/g' > cur_preprints_head.html
+#cat cur_preprints_head.html published_tail.html > all_our.html
+bibtex2html -r -s "plain" --no-header --no-footer -t "Published papers in refereed journals" published.bib
+bibtex2html -r -s "plain" --no-header --no-footer -t "Recent preprints" cur_preprints.bib
+cat template-header.html cur_preprints.html published.html template-footer.html > final.html
+
