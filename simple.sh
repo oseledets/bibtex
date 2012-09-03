f476f35695d4ddd7874460c0a6fb6a1baf360a3b
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-09-03 12:37:08 +0400

    Added thanks file with grants

--- simple.sh
+++
@@ -1,3 +0,0 @@
-#bibmanage.py --search=Oseledets:author -b 2003 -e 2012 --keep-keys --sort=year our.bib -o oseledets.bib -f bib
-#bibmanage.py --search=Oseledets:author -b 2003 -e 2012 --keep-keys --sort=year our.bib -o oseledets.bib -f bib
-
