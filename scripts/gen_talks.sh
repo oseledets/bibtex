2ec9dcd3073c6ec838de61fd87eac8a6f692d370
Ivan Oseledets <ivan.oseledets@gmail.com>
2014-11-17 11:10:18 +0300

    IEEE CAD paper published

--- scripts/gen_talks.sh
+++ scripts/gen_talks.sh
@@ -1,9 +1,7 @@
 #bib2bib -ob kias2.bib -c 'note : "Plenary speaker" or note : "Пленарный доклад"' talks.bib
 #bib2bib -ob kias.bib -c 'note : "Plenary speaker" or note : "Invited speaker" or note : "Пленарный доклад" or note : "Приглашенный доклад"' talks.bib
-bib2bib -ob kias2.bib -c 'note : "Plenary speaker" or note : "Пленарный доклад"' talks.bib
-bib2bib -ob kias.bib -c 'note : "Plenary speaker" or note : "Invited speaker" or note : "Пленарный доклад" or note : "Приглашенный доклад"' talks.bib
-bib2bib -ob talks2013.bib -c 'year = 2013' talks.bib
-bibtex2html -nolinks -r -nobibsource talks2013.bib
+bib2bib -ob talks3yr.bib -c 'year > 2011 and author : "Оселедец"'   talks.bib
+bibtex2html -nolinks -r -nobibsource talks3yr.bib
 #sed "s/Plenary speaker/Пленарный доклад/g" kias.html > 5.html
 #mv 5.html kias.html
 #sed "s/Invited speaker/Приглашенный доклад/g" kias.html > 5.html
